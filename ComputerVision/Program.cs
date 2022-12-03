5bfd956ed0f4f3fecaac9d75b043e86bdba68d3c
Shayan <shayan@zencode.io>
2022-12-03 18:32:36 +0100

    Add project files.

---
+++ ComputerVision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerVision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
